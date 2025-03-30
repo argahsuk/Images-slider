3cb7dbfdd425dcc40a73009d4b68d9e190055548
kushagra <agrakush4@gmail.com>
2025-03-31 00:08:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+
+let scrollStep = window.innerWidth <= 768 ? 252 : 400;
+
+function updateScrolls() {
+    scrolls = window.innerWidth <= 768 ? 252 : 400;
+}
+
+
+
+
+
 let mainbox=document.getElementById('imagediv')
 let images=document.querySelectorAll('.slider-image')
 let x=1;
 
 //scrolling
 document.getElementById('left').addEventListener('click',function(){
-    mainbox.scrollLeft-=400
+    mainbox.scrollLeft-=scrolls
     x--;
 
     //setting buttons hiding
@@ -17,9 +28,8 @@ document.getElementById('left').addEventListener('click',function(){
     }
 })
 
-
 document.getElementById('right').addEventListener('click',function(){
-    mainbox.scrollLeft+=400
+    mainbox.scrollLeft+=scrolls
     x++;
      //setting buttons hiding
     if (x==21) {
@@ -30,3 +40,20 @@ document.getElementById('right').addEventListener('click',function(){
     document.getElementById('left').style.visibility="visible"
     }
 })
+
+
+//media query in js 
+
+// Media query listener
+let mediaQuery = window.matchMedia("(max-width: 768px)");
+
+function handleMediaChange(e) {
+    updateScrolls(); 
+    console.log("Screen size changed. Scroll step:", scrolls);
+}
+
+// Check on initial load
+handleMediaChange(mediaQuery);
+
+// Listen for media query changes
+mediaQuery.addEventListener("change", handleMediaChange);
